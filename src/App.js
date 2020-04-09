4b9fc1beecf3753e0d22d43d0148640f29b0d549
Ciobanu Ioana <ciobanuioana@192-168-0-112.rdsnet.ro>
2020-04-09 22:48:11 +0300

    2.2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { searchRequest, nutrientsRequest } from './requests';
 
+const essentials = ["Protein", "Total lipid (fat)", "Carbohydrate, by difference", "Water", "Calcium, Ca", "Iron, Fe", "Magnesium, Mg", "Sugars, total"];
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -42,7 +44,8 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state.items);
+    console.log("Iteeems:",this.state.items);
+    
     const {items, nutrients} = this.state;
     return (
       <div className="page">
@@ -89,24 +92,34 @@ class Item extends React.Component {
     
     const {currentItem, nutrients} = this.props;
     const nutrientsList = currentItem.full_nutrients.map((currentNutrient) => {
-      const foundNutrient = nutrients.find(nutrient => nutrient.attr_id === currentNutrient.attr_id);
+      const foundNutrient = nutrients.find(nutrient => (nutrient.attr_id === currentNutrient.attr_id && currentNutrient.value !== 0));
       if(foundNutrient === undefined) return null;
       return this.generateObject(foundNutrient.usda_nutr_desc, currentNutrient.value, foundNutrient.unit);
     }).filter(nutrient => nutrient !== null);
 
+    const energyKcal = nutrientsList.find(nutrient => (nutrient.name === "Energy" && nutrient.unit === "kcal"));
+    const energyKJ = nutrientsList.find(nutrient => (nutrient.name === "Energy" && nutrient.unit === "kJ"));
+    const newNutrientsList = nutrientsList.filter( nutrient => nutrient.name !== "Energy");
+    const essentialNutrients = newNutrientsList.filter(nutrient => essentials.indexOf(nutrient.name) !== -1);
+    
+
+
     return (
       <div className="single-item">
         <div className="top-part">
-          <div className="item-title">{this.props.currentItem.food_name}</div>
+          <div className="left-part">
+            <div className="item-title">{this.props.currentItem.food_name}</div>
+            <div className="energy">Energy:{energyKcal.quantity} {energyKcal.unit}/{energyKJ.quantity} {energyKJ.unit}</div>
+          </div>
           <img alt="item" src={this.props.currentItem.photo.thumb} /> 
         </div>
         <div className= "nutrients-table">
         {
-            nutrientsList.map((currentItem) => (
-              <table>
+            essentialNutrients.map((currentItem) => (
+              <table className="nutrient">
                 <tbody>
                   <tr>
-                    <td>{currentItem.name}</td>
+                    <td>{currentItem.name}: </td>
                     <td>{currentItem.quantity}</td>
                     <td>{currentItem.unit}</td>
                   </tr>
